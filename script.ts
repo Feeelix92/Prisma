c730e33f32d0d6a29614e0c6977f1b64029283fd
Felix Maurer <Feeelix92@users.noreply.github.com>
2022-08-29 15:52:11 +0200

    added script functions to get data from database

--- script.ts
+++ script.ts
@@ -4,7 +4,24 @@ const prisma = new PrismaClient()
 
 async function main() {
 
-    const artikel = await prisma.artikel.findMany()
+    // Alle Artikel suchen
+    // const artikel = await prisma.artikel.findMany()
+
+    // Alle roten Artikel suchen
+    const artikel = await prisma.artikel.findMany({
+        where: {
+            farbe: 'rot',
+        }
+    })
+
+    // Einen bestimmten Artikel suchen
+    // const artikel = await prisma.artikel.findUnique({
+    //     where: {
+    //         artnr: '100001',
+    //     }
+    // })
+
+    // Ausgabe der Suchmenge in der console
     console.log(artikel)
 }
 
